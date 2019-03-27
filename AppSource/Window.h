e63577ef9b0f659786435972a98a09acbc5dfb42
blacky <xblackypl@gmail.com>
2019-03-27 19:43:30 +0100

    Adding view tread

--- AppSource/Window.h
+++ AppSource/Window.h
@@ -2,15 +2,21 @@
 #include "IWindow.h"
 
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <thread>
+#include <future>
 
 namespace CoreUtils
 {
 	class Window : public IWindow
 	{
 	private:
-		std::unique_ptr<sf::RenderWindow> window_;
+		std::thread view_;
 		WindowStyles window_style_;
-		sf::ContextSettings settings_;
+		sf::ContextSettings settings_;		
+		std::unique_ptr<sf::RenderWindow> window_;
+		std::atomic<bool> running_view_{ false };
 	public:
 		explicit Window(WindowStyles);		
 		auto close() -> void override;
@@ -25,6 +31,10 @@ namespace CoreUtils
 		auto setStyle(WindowStyles) -> void override;
 		auto getMousePosition() const->sf::Vector2i override;
 
-		// ~Window() = default;
+		auto calculateView(const CUDAHelpers::ComputingData&) -> void override;
+		auto constructImageFromVector(const CUDAHelpers::ComputingData&) const -> sf::Image override;
+		auto setActive(bool) const -> void override;
+
+		~Window();	
 	};
 }
